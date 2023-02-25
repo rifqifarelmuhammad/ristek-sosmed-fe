af97363b10d84872bf7221c1e2e0d29d297e6495
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-25 13:11:34 +0700

    Handling a unexpected error in deployment

--- pages/api/authentication/[email].ts
+++ pages/api/authentication/[email].ts
@@ -3,6 +3,8 @@ import axios from "axios"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const { email } = req.query;
+    console.log('masuk')
+    console.log(email)
     axios.get(`http://localhost:8000/users/${email}`, req.body).then((response) => {
         res.status(200).json(response.data)
     }).catch((error) => {
